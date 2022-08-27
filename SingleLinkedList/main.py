8fa3b1b5b827cf59096d02a0b475f95dda9eebdc
dominhdang148 <myclone235@gmail.com>
2022-08-27 23:52:28 +0700

    Hoàn thành Thưviện DSLK đơn, chuẩn bị tạo chức năng

--- SingleLinkedList/main.py
+++ SingleLinkedList/main.py
@@ -1,18 +1,27 @@
-from os import system
-from menu import MenuClass
-import numpy as np
-choice = '123'
-menu = MenuClass()
-while choice != 0:
-    choice = menu.ChonMenu()
-    menu.XuLyMenu(choice)
-system('cls')
+# from os import system
+# from menu import MenuClass
+# import numpy as np
+# choice = '123'
+# menu = MenuClass()
+# while choice != 0:
+#     choice = menu.ChonMenu()
+#     menu.XuLyMenu(choice)
+# system('cls')
 
-# from singlyLInkedList import SinglyLinkedList
+from singlyLInkedList import SinglyLinkedList
 
-# words = SinglyLinkedList()
-# words.Append("a")
-# words.Append("b")
-# words.Append("c")
+words = SinglyLinkedList()
+# words.append("a")
+# words.append("b")
+# words.append("c")
+
 # for word in words.iter():
 #     print(word)
+
+# print("The size of the current list: {}".format(words.size))
+print("Inserting 'y' in the first line")
+words.insert_after('y', 't')
+
+for word in words.iter():
+    print(word)
+print("The size of the current list: {}".format(words.size))
--- SingleLinkedList/singlyLInkedList.py
+++ SingleLinkedList/singlyLInkedList.py
@@ -7,7 +7,7 @@ class SinglyLinkedList:
         self.head = None
         self.size = 0
 
-    def Append(self, data):
+    def append(self, data):
         node = Node(data)
         if self.head:
             self.head.next = node
@@ -37,4 +37,48 @@ class SinglyLinkedList:
                 return
             prev = current
             current = current.next
-    
+
+    def search(self, data):
+        for node in self.iter():
+            if node == data:
+                return True
+            return False
+
+    def clear(self):
+        self.tail = None
+        self.head = None
+        self.size = 0
+
+    def isEmpty(self):
+        return self.tail == None and self.head == None
+
+    def insert_start(self, data):
+        if self.isEmpty():
+            self.append(data)
+            return
+        new_node = Node(data)
+        new_node.next = self.tail
+        self.tail = new_node
+        self.size += 1
+
+    def insert_after(self, data, previous_data):
+        # Case 1: Không có node nào  trong danh sách
+        if self.isEmpty():
+            self.append(data)
+            return
+        current = self.tail
+        while current:
+            if current.data == previous_data:
+                node = Node(data)
+                # Case 3: previous node là node cuối --> append như thông thường
+                if current == self.head:
+                    self.append(data)
+                else:
+                # Case 4: previous node là node đầu hoặc giữa --> thay đổi liên kết
+                    node.next = current.next
+                    current.next = node
+                    self.size += 1
+                return
+            current = current.next
+        # Case 2: Không tìm thấy previous node --> chèn ở đầu ds
+        self.insert_start(data)
